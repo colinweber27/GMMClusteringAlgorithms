2688e9222b6d12965e2c8c853607349982c439ae
colinweber27 <colin.weber.27@gmail.com>
2021-02-12 20:54:10 -0600

    Version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="GMMClusteringAlgorithms",
-    version="0.1.4",
+    version="0.1.5",
     author="Colin Weber",
     author_email="colin.weber.27@gmail.com",
     url='https://pypi.org/project/GMMClusteringAlgorithms/',
